1b77acc7d920a3a172063efedd8db3d3a222d535
pumukun <demgrisha@gmail.com>
2023-10-09 22:19:40 +0300

    made ls ll

--- LinkedList_Ts.js
+++ LinkedList_Ts.js
@@ -1,44 +1,36 @@
-class llNode {
-    data: any;
-    next: llNode;
-
-    constructor(pdata: any) {
+var llNode = /** @class */ (function () {
+    function llNode(pdata) {
         this.data = pdata;
     }
-}
-
-class LinkedList {
-    head: llNode;
-
-    constructor() {
+    return llNode;
+}());
+var LinkedList = /** @class */ (function () {
+    function LinkedList() {
         this.head = null;
     }
-
-    insert(pdata: any) {
+    LinkedList.prototype.insert = function (pdata) {
         var newNode = new llNode(pdata);
         if (this.head === null) {
             this.head = newNode;
             return;
         }
-
         var tmpNode = this.head;
         while (tmpNode.next !== null) {
             tmpNode = tmpNode.next;
         }
         tmpNode.next = newNode;
         return;
-    }
-
-    pop() {
+    };
+    LinkedList.prototype.pop = function () {
         if (this.head === null) {
             return;
         }
-
         var tmpNode = this.head;
         while (tmpNode.next !== null) {
             tmpNode = tmpNode.next;
         }
         tmpNode = null;
         return;
-    }
-}
+    };
+    return LinkedList;
+}());
